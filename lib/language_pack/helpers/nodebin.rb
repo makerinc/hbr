ef288f01ca4fdf33b453a7ee4bbbca859174febe
Pavel Linkesch <pavel.linkesch@gmail.com>
2024-06-18 17:28:39 +0200

    downgrade yarn

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -10,7 +10,7 @@ class LanguagePack::Helpers::Nodebin
   end
 
   def self.hardcoded_yarn
-    version = "3.2.0"
+    version = "1.22.15"
     {
       "number" => version,
       "url"    => "https://s3.amazonaws.com/heroku-nodebin/yarn/release/yarn-v#{version}.tar.gz"
